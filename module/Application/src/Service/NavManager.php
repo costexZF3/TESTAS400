baee5213551f5187b27ee1ff10bb9e7598369a26
Michel Ojeda <mojeda@costex.com>
2018-08-21 14:16:50 -0400

    updating NavManager: creating addMenuOptions() method

--- module/Application/src/Service/NavManager.php
+++ module/Application/src/Service/NavManager.php
@@ -25,15 +25,142 @@ class NavManager
      */
     private $rbacManager;
     
+    
+    /**
+     * Entity manager 
+     * @var \DoctrineModule\Service\Authentication\AdapterFactory; 
+     */    
+    private $entityManager;
+    
     /**
      * Constructs the service.
      */
-    public function __construct($authService, $urlHelper, $rbacManager) 
+    public function __construct($authService, $urlHelper, $rbacManager, $entityManager) 
     {
         $this->authService = $authService;
         $this->urlHelper = $urlHelper;
         $this->rbacManager = $rbacManager;
+        //add the options access for this user
+         $this->entityManager = $entityManager;
     }
+    
+    /**
+     * this function return an array with the items will be shown 
+     */
+    private function addMenuOptions(string $menuPermission )
+    {
+        $url = $this->urlHelper;
+        //load the options associate to the user with the $menuPermission 
+        //doit....
+        
+        $options= []; //init $optionss       
+        if ($this->rbacManager->isGranted(null, $menuPermission)) {
+            $options[] = [  
+                         'id' => 'claims',
+                         'label' => 'Claims',
+                         'link' => $url('claims',['action'=>'index'])
+                     ];
+              
+            /* Menu Product Developments */
+            $options[] = [
+                        'id' => 'productDevelopment',
+                        'label' => 'Product Developments',
+                        'link' => $url('pagebuilding')
+                    ];
+            /* Menu supplies */ 
+            $options[] = [
+                        'id' => 'supplies',
+                        'label' => 'Supplies',
+                        'link' => $url('pagebuilding')
+                    ];
+                
+            /* Comments, New Supplies/Others */        
+            $options[] = [
+                        'id' => 'comments',
+                        'label' => 'Comments, New Supplies/Others',
+                        'link' => $url('pagebuilding')
+                    ];
+            /* Sales Back Orders */        
+            $options[] = [
+                       'id' => 'salesbackorders',
+                       'label' => 'Sales Backorders',
+                       'link' => $url('pagebuilding')
+                   ];
+                
+                  /* Follow Backorders */        
+            $options[] = [
+                        'id' => 'followbackorders',
+                        'label' => 'Follow Backorders',
+                        'link' => $url('pagebuilding')
+                    ];
+                
+            /* Part/Vendor Comments */        
+            $options[] = [
+                      'id' => 'partvendorcomments',
+                      'label' => 'Part/Vendor Comments',
+                      'link' => $url('pagebuilding')
+                  ];
+                
+                  /* Purchasing Quote */        
+            $options[] = [
+                        'id' => 'partvendorcomments',
+                        'label' => 'Purchasing Quote',
+                        'link' => $url('pagebuilding')
+                    ];
+
+             /* Suspended Parts*/        
+            $options[] = [
+                        'id' => 'suspendedparts',
+                        'label' => 'Suspended Parts',
+                        'link' => $url('pagebuilding')
+                    ];
+
+             /* Upload OEM Pictures */        
+            $options[] = [
+                        'id' => 'uploadoempictures',
+                        'label' => 'Upload OEM Pictures',
+                        'link' => $url('pagebuilding')
+                    ];
+
+             /* Email Vendors */        
+            $options[] = [
+                        'id' => 'emailvendors',
+                        'label' => 'Email Vendors',
+                        'link' => $url('pagebuilding')
+                    ];
+
+             /* Vendors Price List */        
+            $options[] = [
+                        'id' => 'vendorspricelist',
+                        'label' => 'Vendors Price List',
+                        'link' => $url('pagebuilding')
+                    ];
+
+             /* Print labels (Vendors)*/        
+            $options[] = [
+                        'id' => 'printlabels',
+                        'label' => 'Print Labels(Vendors)',
+                        'link' => $url('pagebuilding')
+                    ];
+
+             /* Change Pur. Agent / Person in charge  */        
+            $options[] = [
+                        'id' => 'changeagentpersonincharge',
+                        'label' => 'Change Pur. Agent/Person in charge',
+                        'link' => $url('pagebuilding')
+                    ];
+             /* Reports */        
+            $options[] = [
+                        'id' => 'reports',
+                        'label' => 'Reports',
+                        'link' => $url('pagebuilding')
+                    ];
+                   
+        } //end if access() to $permission 
+        
+        return $options;
+    } //End: method addMenuOptions
+    
     
     /**
      * This method returns menu items depending on whether user has logged in or not.
@@ -58,12 +185,10 @@ class NavManager
             'id' => 'home',
             'label' => 'Home',
             'link'  => $url('home')
-        ];
-        
+        ];        
                
         // Display "Login" menu item for not authorized user only. On the other hand,
-        // display "Admin" and "Logout" menu items only for authorized users.
-        
+        // display "Admin" and "Logout" menu items only for authorized users.       
         
         if (!$this->authService->hasIdentity()) {
             $items[] = [
@@ -74,142 +199,38 @@ class NavManager
             ];
         } else {
             
-            /*
-             ********************************************************************
-             *  Determine which items must be displayed in Purchasing           *
-             * ******************************************************************
-             */            
-            
-            $purchasingDropdownItems = [];
-            //call method for creating all menus dinamically
-            
-            if ($this->rbacManager->isGranted(null, 'menu.purchasing')) {
-                $purchasingDropdownItems[] = [
-                            'id' => 'claims',
-                            'label' => 'Claims',
-                            'link' => $url('claims',['action'=>'index'])
-                        ];
-              
-                /* Menu Product Developments */
-                $purchasingDropdownItems[] = [
-                            'id' => 'productDevelopment',
-                            'label' => 'Product Developments',
-                            'link' => $url('pagebuilding')
-                        ];
-                /* Menu supplies */ 
-                $purchasingDropdownItems[] = [
-                            'id' => 'supplies',
-                            'label' => 'Supplies',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                /* Comments, New Supplies/Others */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'comments',
-                            'label' => 'Comments, New Supplies/Others',
-                            'link' => $url('pagebuilding')
-                        ];
-                 /* Sales Back Orders */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'salesbackorders',
-                            'label' => 'Sales Backorders',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                  /* Follow Backorders */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'followbackorders',
-                            'label' => 'Follow Backorders',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                  /* Part/Vendor Comments */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'partvendorcomments',
-                            'label' => 'Part/Vendor Comments',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                  /* Purchasing Quote */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'partvendorcomments',
-                            'label' => 'Purchasing Quote',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                 /* Suspended Parts*/        
-                $purchasingDropdownItems[] = [
-                            'id' => 'suspendedparts',
-                            'label' => 'Suspended Parts',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                 /* Upload OEM Pictures */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'uploadoempictures',
-                            'label' => 'Upload OEM Pictures',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                 /* Email Vendors */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'emailvendors',
-                            'label' => 'Email Vendors',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                 /* Vendors Price List */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'vendorspricelist',
-                            'label' => 'Vendors Price List',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                 /* Print labels (Vendors)*/        
-                $purchasingDropdownItems[] = [
-                            'id' => 'printlabels',
-                            'label' => 'Print Labels(Vendors)',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                 /* Change Pur. Agent / Person in charge  */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'changeagentpersonincharge',
-                            'label' => 'Change Pur. Agent/Person in charge',
-                            'link' => $url('pagebuilding')
-                        ];
-                 /* Reports */        
-                $purchasingDropdownItems[] = [
-                            'id' => 'reports',
-                            'label' => 'Reports',
-                            'link' => $url('pagebuilding')
-                        ];
-                
-                
-            }//end: rbacManager->isGrandted(null, 'purchasing.menu'), CHECKS IF THE LOGIN USER HAS THE purchasin.menu PERMISSION
-            
-            
+            /**
+             * CREATE DYNAMIC MENUS WITH THE OPTIONS GIVEN OR ASSIGNED EACH MENU 
+             * Management, Marketing, MIS, Purchasing, Quality Control, Manufacturing, Sales Shipping
+             *  Recesiving, Warehouse, maintenance 
+             */       
+                        
+            // Determine which items must be displayed in Purchasing           *
+           $purchasingMenuOptions = $this->addMenuOptions('menu.purchasing');
+           
+            //----- THE LINES BELOW MUTS BE DYNAMICALLY IMPLEMENTED ----
             //************ RENDERING PURCHASING dropDownItems WITH ALL OPTIONS *******************
             //checking whether purchasing's menu-items is different of 0 
-            if (count($purchasingDropdownItems)!=0) {
+            if (count($purchasingMenuOptions)!=0) {
                 $items[] = [
                     'id' => 'purchasing', 
                     'label' => 'Puchasing',
-                    'dropdown' => $purchasingDropdownItems
+                    'dropdown' => $purchasingMenuOptions
                 ];
-            }
+            }  
             
              /*
-             ********************************************************************
-             *  Determine which items must be displayed in Admin dropDownList   *
-             * ******************************************************************
+             ****************************************************************************
+             *  Determine WHAT items(OPTIONS) must be displayed in Admin dropDownList   *
+             * **************************************************************************
              */            
             
             // ARRAY WITH ITEMS FOR ADMIN USERS
-            $adminDropdownItems = [];
-            
-            if ($this->rbacManager->isGranted(null, 'user.manage')) {
-                $adminDropdownItems[] = [
+            $adminMenuOptions = [];
+            $has =  $this->rbacManager->isGranted(null, 'user.manage');
+            var_dump($has);
+            if ($this->rbacManager->isGranted(null, 'user.manage')) {                
+                $adminMenuOptions[] = [
                             'id' => 'users',
                             'label' => 'Manage Users',
                             'link' => $url('users')
@@ -217,7 +238,7 @@ class NavManager
             }
             
             if ($this->rbacManager->isGranted(null, 'permission.manage')) {
-                $adminDropdownItems[] = [
+                $adminMenuOptions[] = [
                             'id' => 'permissions',
                             'label' => 'Manage Permissions',
                             'link' => $url('permissions')
@@ -225,18 +246,18 @@ class NavManager
             }
             
             if ($this->rbacManager->isGranted(null, 'role.manage')) {
-                $adminDropdownItems[] = [
+                $adminMenuOptions[] = [
                             'id' => 'roles',
                             'label' => 'Manage Roles',
                             'link' => $url('roles')
                         ];
             }
             //checking whether Admin's menu-items is different of 0 
-            if (count($adminDropdownItems)!=0) {
+            if (count($adminMenuOptions)!=0) {
                 $items[] = [
                     'id' => 'admin',
                     'label' => 'Admin',
-                    'dropdown' => $adminDropdownItems
+                    'dropdown' => $adminMenuOptions
                 ];
             }
             
