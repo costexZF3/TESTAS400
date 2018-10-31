e4e037ef16c1bbabc7ff3d89fceb788ea0be5c58
CostexZF3 <mojeda@costex.com>
2018-10-31 09:31:43 -0400

    Add Service: MyAdapter.php and its  Factory: MyAdapterFactory

---
+++ module/Application/src/Service/Factory/MyAdapterFactory.php
@@ -0,0 +1,38 @@
+<?php
+namespace Application\Service\Factory;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+use Zend\Db\Adapter\Adapter;
+use Application\Service\MyAdapter;
+
+
+/**
+ * This is the factory for IndexController. Its purpose is to instantiate the
+ * controller and inject dependencies into IndexController
+ */
+class MyAdapterFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+
+           $dbconnection = [
+                        'driver'         => 'IbmDb2',
+                        'database'       => 'COSTEX1',// COSTEXM15  DEVELOPMENT
+                        'hostname'      =>  'COSTEXM15',
+                        'username'       => 'mojeda',
+                        'password'       => 'michelo',
+                        'driver_options' => [
+                            //'i5_commit' => DB2_I5_TXN_READ_UNCOMMITTED,
+                            'autocommit' => DB2_AUTOCOMMIT_OFF,
+                            'i5_lib' => "QS36F",
+                        ],
+                   ];
+
+        $conAdapter= new Adapter( $dbconnection );
+       
+        //------- Instantiate the controller and inject dependencies
+        return new MyAdapter( $conAdapter );
+    }
+}
---
+++ module/Application/src/Service/MyAdapter.php
@@ -0,0 +1,21 @@
+<?php 
+ namespace Application\Service;
+ 
+ use Zend\Db\Adapter\Adapter;
+ 
+ class MyAdapter
+ {    
+    
+     private $conn;
+     
+     public function __construct( Adapter $adapter )
+     {
+         $this->conn = $adapter;
+     }
+     
+     public function getAdapter(){
+         return $this->conn;
+     }
+     
+ }
+?>
