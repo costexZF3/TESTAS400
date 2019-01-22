22441d789f2c7cb959119bee768afe23d8d645aa
CostexZF3 <mojeda@costex.com>
2019-01-22 11:00:47 -0500

    Creating LostSale.php class: used to recover lostSale

---
+++ module/Purchasing/src/Object/LostSale.php
@@ -0,0 +1,17 @@
+<?php
+namespace Purchasing\Object;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of LostSale
+ *
+ * @author mojeda
+ */
+class LostSale {
+    //put your code here
+}
