4e651ee9bd7d5119809cec518530ec773702f2ad
Michel Ojeda <mojeda@costex.com>
2019-02-04 15:13:54 -0500

    LostSalesController.php
    - deleting flashmessengers

--- module/Purchasing/src/Controller/LostsaleController.php
+++ module/Purchasing/src/Controller/LostsaleController.php
@@ -9,7 +9,7 @@ use Zend\Db\Adapter\Adapter;
 
 
 use Purchasing\ValueObject\LostSale;
-
+use Purchasing\Form\LostSaleForm;
 
 class LostsaleController extends AbstractActionController
 {
@@ -60,35 +60,58 @@ class LostsaleController extends AbstractActionController
    /**
     *  The IndexAction show the main Menu about all concerning to the Purchasing Menus
     */
-   public function indexAction(){ 
+   public function indexAction() {
+        //getting the loggin user object
+        $user = self::getUser(); 
+    
+     //-- checking if the logged user has SPECIAL ACCESS: he/she would be an Prod. Specialist
+        $especial = ($this->access('special.access'))?'TRUE':'FALSE';
+       
+        $changeData = true;
 
-    $this->flashMessenger()->addInfoMessage('The shown data are based on the following criteria: TimesQuote: +100, Vendors Assigned: YES ');
+      //create LostSaleForm
+        $form = new LostSaleForm();
+
+        //check if user has submitted the form
+
+        if ($this->getRequest()->isPost()) {
+            //fill in the form with POST data        
+            $data = $this->params()->fromPost();            
+//            
+//            $form->setData($data);
+//            
+//            if ($form->isValid()) {
+//                $data = $form->getData(); //getting back data filter and validated 
+//                echo "valid data";  
+//            }
+            $timesQuote = (int)$data['num-tq'];  //getting times quoted   
+            $changeData =  ($data['oldtimesquoted']!= $timesQuote)?? false;
+            
+            if ($changeData) {
+                $msg = 'Criteria for times Quote has chanced. Parts  with more than '.$timesQuote.' times quote are being shown';                      
+               
+            }
+        } else {
+           $msg = 'The shown data are based on the following criteria: TimesQuote: +100, Vendors Assigned: YES ';            
+           //Initicial Value for TimesQuote 
+           $timesQuote = 100;  
+           
+        }
          
-    //getting the loggin user object
-    $user = self::getUser();  
-              
-        //-- checking if the logged user has SPECIAL ACCESS: he/she would be an Prod. Specialist
-       $especial = ($this->access('special.access'))?'TRUE':'FALSE';
-       
-       //Initicial Value for TimesQuote 
-       $timesQuote = "50"; 
-       
-       /* LostSale: It's an Object that will be the core of LostSales  */
-       $LostSale = new LostSale($this->conn, $timesQuote);
-       
-       /* this method retrives all items and return a resultSet or data as HTML tableGrid */
-       $resultSet = $LostSale->getDataSet(); 
-       $tableHTML= $LostSale->getGridAsHtml();
+       /* this method retrives all items and return a resultSet or data as HTML tableGrid */   
+       $LostSale = new LostSale( $this->conn, $timesQuote );
+       $tableHTML = $LostSale->getGridAsHtml();
        $countItems = $LostSale->getCountItems();
+       $timesQuote = $LostSale->getTimesQuoted(); 
        
        $this->layout()->setTemplate('layout/layout_Grid');
        return new ViewModel([
-                     'tableHeader' => $tableHTML,
-                    'lostsalelist' => $resultSet,                                            
+                            'form' => $form,         //HTML ELEMENTS: to render on the filter seccions
+                     'tableHeader' => $tableHTML,                                                                    
                             'user' => $user,
                    'specialAccess' => $especial,
                       'countItems' => $countItems,
-                   
+                     'timesquoted' => $timesQuote,
             ]);
     }//END: indexAction method
     
